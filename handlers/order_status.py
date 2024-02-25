1a0a5d0b991fe4c25baf7f7a04af2643d1677bce
puzikov <bythetort@gmail.com>
2024-02-25 16:14:10 +0300

    changed settings

--- handlers/order_status.py
+++ handlers/order_status.py
@@ -71,7 +71,7 @@ async def get_sale(callback_query: CallbackQuery, state: FSMContext, bot: Bot):
 
 @router.message(F.content_type.in_({"contact"}))
 async def authorize(message: Message, state: FSMContext):
-    phone_number = '79244996888' or message.contact.phone_number
+    phone_number = message.contact.phone_number
     await set_to(str(message.from_user.id), str(phone_number), ex=3600)
     await message.answer(
         "Спасибо! Теперь мы сможем найти Ваши заказы",
--- log_settings.py
+++ log_settings.py
@@ -1,7 +1,7 @@
 import logging
 log = logging.getLogger('logger')
 log.setLevel(logging.DEBUG)
-fh = logging.FileHandler('app.log', 'a', 'utf-8')
+fh = logging.FileHandler('log/app.log', 'a', 'utf-8')
 formatter = logging.Formatter('%(levelname)s - %(asctime)s - %(message)s')
 fh.setFormatter(formatter)
 log.addHandler(fh)
