314c18a7185cc30e1b463a365db12c06ad943be2
puzikov <bythetort@gmail.com>
2024-03-10 12:09:22 +0300

    added new func for uploading files to server

--- integration/helpers.py
+++ integration/helpers.py
@@ -1,5 +1,10 @@
-from log_settings import log
+import os
+from io import BytesIO
+
+import aiohttp
+
 from configuration import DELIVERY_MSG_CFG
+from log_settings import log
 
 
 def get_status_filters(actuality: str) -> str:
@@ -17,6 +22,20 @@ def get_status_filters(actuality: str) -> str:
 def get_message_mapping_config(
         codes_only: bool = False, categories: tuple = ()
 ) -> dict:
-
     codes = {key for key, val in DELIVERY_MSG_CFG.items() if val.get('category') in categories}
     return codes if codes_only else DELIVERY_MSG_CFG
+
+
+async def upload_photo_to_server(file_io, file_name):
+    url = os.getenv('UPLOAD_PHOTO_URL')
+
+    async with aiohttp.ClientSession() as session:
+        try:
+            with BytesIO(file_io.getvalue()) as f:
+                photo_bytes = f.read()
+            files = {'file': photo_bytes, 'file_name': '' or file_name}
+            async with session.post(url, data=files, raise_for_status=True) as response:
+                return await response.text()
+        except Exception as e:
+            log.error('Error while uploading photo to yourcstm.space, exc = %s', e)
+            return
