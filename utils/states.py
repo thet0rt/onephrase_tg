ea79c9cd6dc53bc471586bc8bceb6e7e2d1b131b
puzikov <bythetort@gmail.com>
2024-03-17 17:33:22 +0300

    added states description

--- utils/states.py
+++ utils/states.py
@@ -2,8 +2,18 @@ from aiogram.fsm.state import StatesGroup, State
 
 
 class CurrentLogic(StatesGroup):
+    # for order_status
     order_status = State()
     order_history = State()
+
+    # for input_order_number
     input_order_number = State()
+    # sets when /start
     basic = State()
+
+    # for loading_photos
     load_photo = State()
+
+    # for b2c/b2b info_logic
+    b2c_logic = State()
+    b2b_logic = State()
